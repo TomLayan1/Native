e0b3da7acb977402935043975497603745185e9b
TomLayan1 <afolayantomisin1919@gmail.com>
2024-11-02 11:37:35 +0100

    Learned the View component and use inline styling to style

--- mobile-app-components/App.tsx
+++ mobile-app-components/App.tsx
@@ -3,8 +3,13 @@ import { Text, View } from "react-native";
  
  export default function App() {
   return(
-    <View>
-      <Text>Hello world!</Text>
+    <View style={{ flex: 1, backgroundColor: 'black'}}>
+      <View style={{ flexDirection: 'row', backgroundColor: 'plum'}}>
+        <View style={{ height: 200, width: 200, backgroundColor: 'blue'}}></View>
+        <View style={{ height: 200, width: 200, backgroundColor: 'green'}}>
+          <Text>Hello world!</Text>
+        </View>
+      </View>
     </View>
   )
  }
