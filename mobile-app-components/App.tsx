04420dc80ca11a27a8cf50f7551456791b2466e6
TomLayan1 <afolayantomisin1919@gmail.com>
2024-11-02 12:29:28 +0100

    Learn about Image components

--- mobile-app-components/App.tsx
+++ mobile-app-components/App.tsx
@@ -1,10 +1,15 @@
-import { Text, View } from "react-native";
+import { Image, ImageBackground, Text, View } from "react-native";
+const logoImage = require('./assets/adaptive-icon.png');
 
  
  export default function App() {
   return(
     <View style={{ flex: 1, backgroundColor: 'black'}}>
-      <Text style={{ color: 'white', fontSize: 16, fontWeight: 'bold' }}>Hello world!</Text>
+      {/* <Image source={logoImage} style={{ width: 100, height: 100 }} />
+      <Image source={{ uri: 'https://gratisography.com/wp-content/uploads/2024/10/gratisography-cool-cat-800x525.jpg'}} style={{ width: 100, height: 100 }} /> */}
+      <ImageBackground source={logoImage} style={{ flex: 1 }}>
+        <Text style={{ color: 'plum', fontSize: 16, fontWeight: 'bold' }}>Hello world!</Text>
+      </ImageBackground>
     </View>
   )
  }
